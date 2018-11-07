f0e7e5181d59074f7c3a1c9921d59e1bd3f93d55
Pixxl <manbearpixel@gmail.com>
2018-11-07 21:52:22 +0000

    Set network params to match ODIN

--- src/networks.js
+++ src/networks.js
@@ -3,25 +3,25 @@
 
 module.exports = {
   bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'bc',
+    messagePrefix: '\x18Huginn hath Signed Message:\n',
+    bech32: 'odin',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
+      public: 0x27561872,
+      private: 0x27256746
     },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
-    wif: 0x80
+    pubKeyHash: 0x73,
+    scriptHash: 0x39,
+    wif: 0x8a
   },
   testnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'tb',
+    messagePrefix: '\x18Huginn hath Signed Message:\n',
+    bech32: 'odt',
     bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+      public: 0x3a8061a0,
+      private: 0x3a805837
     },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
+    pubKeyHash: 0x89,
+    scriptHash: 0x19,
     wif: 0xef
   }
 }
